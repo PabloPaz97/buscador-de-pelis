637917a81e93d8c7ca1e0c82775d02281d469f28
Pablo Paz <106879560+PabloPaz97@users.noreply.github.com>
2025-04-25 17:21:14 -0300

    index.js

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ function main() {
   const args = process.argv.slice(2); // ejemplo: ["--sort", "title"]
 
   if (args.length === 0) {
-    console.table(pelis.getAll());
+    console.log(pelis.getAll());
     return;
   }
 
   if (args.includes("--sort")) {
     const prop = args[args.indexOf("--sort") + 1];
-    console.table(pelis.sortBy(prop));
+    console.log(pelis.sortBy(prop));
   }
 
   if (args.includes("--search")) {
     const palabra = args[args.indexOf("--search") + 1];
-    console.table(pelis.searchByTitle(palabra));
+    console.log(pelis.searchByTitle(palabra));
   }
 
   if (args.includes("--tag")) {
     const tag = args[args.indexOf("--tag") + 1];
-    console.table(pelis.filterByTag(tag));
+    console.log(pelis.filterByTag(tag));
   }
 }
 
